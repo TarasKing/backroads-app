02c120f94cd5babc7e1bb82e23575126778865d1
TarasKing <ivanovtaras@live.ru>
2023-03-20 22:27:58 -0700

    created Title, fix Hero, Tours, Services

--- src/data.js
+++ src/data.js
@@ -1,3 +1,10 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+import tour5 from "./images/tour-5.jpeg";
+import tour6 from "./images/tour-6.jpeg";
+
 export const pageLinks = [
   {
     id: 1,
@@ -36,5 +43,89 @@ export const socialLinks = [
     id: 3,
     href: "https://www.twitter.com",
     icon: "fab fa-squarespace",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    img: tour1,
+    date: "august 26th, 2020",
+    title: "tibet adventure",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    location: "china",
+    duration: "6 days",
+    price: 2100,
+  },
+  {
+    id: 2,
+    img: tour2,
+    date: "october 1th, 2020",
+    title: "best of java",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    location: "indonesia",
+    duration: "11 days",
+    price: 1400,
+  },
+  {
+    id: 3,
+    img: tour3,
+    date: "december 15th, 2020",
+    title: "best of java",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    location: "indonesia",
+    duration: "11 days",
+    price: 1400,
+  },
+  {
+    id: 4,
+    img: tour4,
+    date: "august 10th, 2020",
+    title: "best of australia",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    location: "australia",
+    duration: "20 days",
+    price: 3400,
+  },
+  {
+    id: 5,
+    img: tour5,
+    date: "september 25th, 2021",
+    title: "best of thailand",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    location: "thailand",
+    duration: "7 days",
+    price: 800,
+  },
+  {
+    id: 6,
+    img: tour6,
+    date: "september 15th, 2020",
+    title: "best of indonesia",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+    location: "indonesia",
+    duration: "15 days",
+    price: 2200,
   },
 ];
